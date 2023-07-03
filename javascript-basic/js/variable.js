1725e55946ece069bf6f466fec754c27e5e10e6a
taneichi01 <supesyaru01@outlook.jp>
2023-07-03 11:42:15 +0900

    最初のコミット

---
+++ javascript-basic/js/variable.js
@@ -0,0 +1,15 @@
+//変数の宣言
+
+userName = '侍太郎'
+
+let userNumber = 55;
+
+console.log(userName);
+console.log(userNumber);
+
+//値の再代入
+userName = '侍花子';
+userNumber = 88;
+
+console.log(userName);
+console.log(userNumber);
